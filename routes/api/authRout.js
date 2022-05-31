0c7002e5f33938ba41b23fce231b84ea7c175ace
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2022-05-31 21:23:44 +0300

    done

--- routes/api/authRout.js
+++ routes/api/authRout.js
@@ -4,6 +4,7 @@ const { userValidation } = require("../../middlevares/validate.js");
 const { authCheck } = require("../../middlevares/authCheck.js");
 const { User } = require("../../models/authModel")
 const { registration, login, logout, current } = require("../../models/auth")
+const { uploadMiddleware, changeAvatar } = require("../../models/changeAvatar")
 
 router.post("/signup", userValidation, async (req, res, next) => {
     const userMail = req.body.email
@@ -28,10 +29,27 @@ router.post("/signin", userValidation, async (req, res, next) => {
 router.get("/logout", authCheck, async (req, res, next) => {
     const result = await logout(req.id);
     res.status(204).json({ message: "No Content" });
-  });
+});
 
 router.get("/current", authCheck, async (req, res, next) => {
     const result = await current(req.id);
     res.json({ user: result });
-  });
+});
+
+router.patch("/avatars", [authCheck, uploadMiddleware.single("avatar")],
+    async (req, res, next) => {
+        const result = await changeAvatar(req.file);
+
+        if (!result) {
+            return res.status(401).json({ message: "Not authorized" });
+        }
+        const avatarUrl = await User.findOneAndUpdate(
+            { _id: req.user.id },
+            { avatarURL: result }
+        );
+
+        res.json({ avatarURL: `${result}` });
+    }
+);
+
 module.exports = router;
